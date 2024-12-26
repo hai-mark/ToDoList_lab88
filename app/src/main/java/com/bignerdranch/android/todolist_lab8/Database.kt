170ab5a4bb32a9eb3a00b39858710046b3d9bdc5
hai-mark <errrorfour@gmail.com>
2024-12-27 01:53:43 +0700

    Изменение Database.kt

--- app/src/main/java/com/bignerdranch/android/todolist_lab8/Database.kt
+++ app/src/main/java/com/bignerdranch/android/todolist_lab8/Database.kt
@@ -1,4 +1,27 @@
 package com.bignerdranch.android.todolist_lab8
 
-class Database {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                val newInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "task_database"
+                ).build()
+                instance = newInstance
+                newInstance
+            }
+        }
+    }
 }
--- app/src/main/java/com/bignerdranch/android/todolist_lab8/Task.kt
+++ app/src/main/java/com/bignerdranch/android/todolist_lab8/Task.kt
@@ -3,11 +3,9 @@ package com.bignerdranch.android.todolist_lab8
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-class Task {
-    @Entity(tableName = "tasks")
-    data class Task(
-        @PrimaryKey(autoGenerate = true) val id: Long = 0,
-        val description: String,
-        val priority: Int // 1 - High, 2 - Medium, 3 - Low
-    )
-}
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val description: String,
+    val priority: Int
+)
